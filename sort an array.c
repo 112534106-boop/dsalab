a4c2dd36ba2b980fd47eb8f33ea803a83a1b15b0
112534106-boop <112534106@kanchiuniv.ac.in>
2026-02-18 02:05:28 -0800

    Add files via upload

---
+++ sort an array.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, n,temp,minIndex;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter %d element:\n",n);
+	for(i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	//selection sort logic
+	for( i=0;i<n-1;i++)
+	{ 
+	minIndex=i;
+	for(j=i+1;j<n;j++)
+	{
+		if(arr[j]<arr[minIndex])
+		minIndex=j;
+	}
+	//swap
+	temp=arr[i];
+	arr[i]=arr[minIndex];
+	arr[minIndex]=temp;
+	}
+	printf("sorted array:\n");
+	for(i=0;i<n;i++)
+	printf("%d",arr[i]);
+	return 0;
+}
